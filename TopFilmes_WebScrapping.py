dd9d707c5e6132deff1fbc216068c0ea3457a876
allanescher <allanbescher@hotmail.com>
2022-06-22 10:02:19 -0300

    ajuste tabela para classificar

--- TopFilmes_WebScrapping.py
+++ TopFilmes_WebScrapping.py
@@ -16,8 +16,9 @@ def trata_valor(valor):
 # Salva os dados encontrados em um arquivo txt
 def salvar(listFilmes):
     arquivo_txt = open('arq_filmes.txt', 'w')
-    for linha in listFilmes:
-        arquivo_txt.write(linha + '\n')
+    arquivo_txt.write(listFilmes)
+    #for linha in listFilmes:
+     #   arquivo_txt.write(linha)
     arquivo_txt.close()
 
 def valida_site(url, listFilmes, contador):
@@ -34,9 +35,10 @@ def valida_site(url, listFilmes, contador):
         # Retorna Vazio caso encontre um erro na URL
         return None
     try:
+        var=[]
         # Transforma a pagina em um objeto BeautifulSoup
         bs = BeautifulSoup(html.read(), 'html.parser')
-        # Encontra a class 'lister-item mode-advanced' e retorna todo o HTML contido nesta
+        # Encontra a class 'lister-item mode-advanced' e retorna o conteúdo HTML contido nesta página
         lista_filmes = bs.find_all('div', {'class': 'lister-item mode-advanced'})
         # Percorre todos o filmes disponiveis na lista da pagina
         for filme in lista_filmes:
@@ -71,7 +73,8 @@ def valida_site(url, listFilmes, contador):
             # Atraves do select, entra na estrutura do HTML para encontrar o nome do filme
             titulo = filme.select('div#main > div > div > div > div > div > h3 > a')[0].text
             # Cria e formata uma variavel com todos os dados coletados do HTML
-            var = ('{:<5}{:<10}{:<10}{:<100}{:<15}{:<10}').format(str(contador), str(num_ibdb), str(metascore), str(titulo), str(votos), str(ano)[:4])
+            #var = ('{:<5}{:<10}{:<10}{:<100}{:<15}{:<10}').format(str(contador), str(num_ibdb), str(metascore), str(titulo), str(votos), str(ano)[:4])
+            var = [str(contador), str(num_ibdb), str(metascore), str(titulo), str(votos), str(ano)[:4]]
             # Salva a variavel na lista
             listFilmes.append(var)
             # Adiciona mais um item no contador da lista
@@ -83,11 +86,15 @@ def valida_site(url, listFilmes, contador):
         return None
 
 # Cria um cabecario para os itens da lista
-listFilmes = ['{:<5}{:<10}{:<10}{:<100}{:<15}{:<10}'.format('#', 'imbd', 'metascore', 'filme', 'votos', 'ano')]
+#listFilmes = ['{:<5}{:<10}{:<10}{:<100}{:<15}{:<10}'.format('#', 'imbd', 'metascore', 'filme', 'votos', 'ano')]
+titulo = ['#', 'imbd', 'metascore', 'filme', 'votos', 'ano']
+listFilmes = []
+listFilmes.append(titulo)
 # Inicia o contador em 0
 contador = 0
 # Percorre as paginas de 50 em 50 filmes até chegar em 2000 filmes.
-for pagina in range(1,2001,50):
+#for pagina in range(1,2001,50):
+for pagina in range(1, 150, 50):
     # URL da pagina
     url = ("https://www.imdb.com/search/title/?release_date=2020-01-01,2022-12-31&sort=num_votes,desc&start={}&ref_=adv_nxt").format(str(pagina))
     # Chama def para encontrar as informacoes desejadas
